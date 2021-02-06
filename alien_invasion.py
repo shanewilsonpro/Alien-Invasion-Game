d89c2d0f2016594cce76da236eab4c00dc4783d1
Shane <shanewilsonpro@gmail.com>
2021-02-05 22:10:19 -0800

    adding and firing bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """
@@ -17,12 +18,13 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        #self.settings.screen_width = self.screen.get_rect().width
+        #self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """
@@ -31,7 +33,9 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
+
 
     def _check_events(self):
         """
@@ -56,6 +60,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """
@@ -66,6 +72,25 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """
+        create a new bullet and add it to the bullets group
+        """
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """
+        update position of bullets and get rid of old bullets
+        """
+        self.bullets.update()
+
+        # get rid of bullets that have disappeared out of screen
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
         """
         update images on the screen and flip to new screen
@@ -73,6 +98,8 @@ class AlienInvasion:
         # redraw screen during each pass thru loop
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # make drawn screen visible
         pygame.display.flip()
--- settings.py
+++ settings.py
@@ -8,9 +8,16 @@ class Settings:
         initialize game's settings
         """
         # screen settings
-        self.screen_width = 600
-        self.screen_height = 400
+        self.screen_width = 1200
+        self.screen_height = 840
         self.bg_color = (230, 230, 230)
 
         # ship settings
         self.ship_speed = 1.5
+
+        # bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 10
